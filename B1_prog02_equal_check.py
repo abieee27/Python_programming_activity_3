7187aadf04947fbdf05f7260bb7676081916a270
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-19 22:13:11 +0800

    add B1_prog02_equal_check

---
+++ B1_prog02_equal_check.py
@@ -0,0 +1,10 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+if num1 == num2:
+  print("equal")
+else: 
+  print("not equal")
