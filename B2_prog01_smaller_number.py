743a3e58af811f3e3eff7bf39f27ef80d4c4db96
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 00:41:38 +0800

    add B2_prog01_smaller_number

---
+++ B2_prog01_smaller_number.py
@@ -0,0 +1,9 @@
+#Abegail Q. fernandez BSCPE1-4
+#B2_Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
+ 
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+if num1 < num2:
+    print("the smaller number is: ", num1)
+else:
+    print("the smaller number is: ", num2)
