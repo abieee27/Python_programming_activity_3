8e44e9227daa6dc929eb0939011d8d0706700c2f
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 01:21:14 +0800

    add B2_prog05_remainder_two_numbers

---
+++ B2_prog05_remainder_two_numbers.py
@@ -0,0 +1,7 @@
+#Abegail Q. fernandez BSCPE1-4
+#B2_Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+remainder = num1 % num2
+print("the remainder is: ", remainder)
