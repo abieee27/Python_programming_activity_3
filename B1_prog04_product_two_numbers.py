ff2be4b8c11d2cf5cc6607da896a120016006c8f
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-19 22:27:57 +0800

    add B1_prog04_product_two_numbers

---
+++ B1_prog04_product_two_numbers.py
@@ -0,0 +1,8 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+product = num1*num2 
+print("the product is: ",product)
