900b639363b8f852f1a00474657fe3fc28d64189
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-19 22:21:29 +0800

    add B1_prog03_sum_two_numbers

---
+++ B1_prog03_sum_two_numbers.py
@@ -0,0 +1,7 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+sum = num1 + num2
+print("the sum is: ", sum)
