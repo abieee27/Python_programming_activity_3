01d99ff23310c1337890f0e003f7b346415a32ec
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 01:11:59 +0800

    add B2_prog04_quotient_no_decimal

---
+++ B2_prog04_quotient_no_decimal.py
@@ -0,0 +1,7 @@
+#Abegail Q. fernandez BSCPE1-4
+#B2_Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+quotient = num1 // num2
+print("the quotient of the two numbers is: ", quotient)
