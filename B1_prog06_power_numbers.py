826fd154a42b35dc7ec3ed474aa52a8a6fb88235
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-19 22:45:58 +0800

    add B1_prog06_power_numbers

---
+++ B1_prog06_power_numbers.py
@@ -0,0 +1,7 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+result = num1 ** num2
+print(f"the result is: {result}")
