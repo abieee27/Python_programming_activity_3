7c378d11d7b11c40a9d41cb6a0cf6199b7917fd4
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-19 22:34:37 +0800

    add B1_prog05_quotient_two_numbers

---
+++ B1_prog05_quotient_two_numbers.py
@@ -0,0 +1,7 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+quotient = num1 / num2
+print("the quotient is: ",quotient)
