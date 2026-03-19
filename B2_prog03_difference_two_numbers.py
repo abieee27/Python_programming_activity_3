9baa63eb99681ec5f696bbdb9bd507d9fbe05eb3
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 01:03:11 +0800

    add B2_prog03_difference_two_numbers

---
+++ B2_prog03_difference_two_numbers.py
@@ -0,0 +1,7 @@
+#Abegail Q. fernandez BSCPE1-4
+#B2_Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter the second number: "))
+difference = num1 - num2
+print("difference of two numbers is: ", difference)
