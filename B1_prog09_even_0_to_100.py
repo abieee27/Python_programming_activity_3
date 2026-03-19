b13ed5ab5d96b59dc43165ea00fe50cbcede7d79
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 00:03:00 +0800

    add B1_prog09_even_0_to_100

---
+++ B1_prog09_even_0_to_100.py
@@ -0,0 +1,6 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+for num in range (0, 101):
+    if num % 2 == 0:
+      print(num)
