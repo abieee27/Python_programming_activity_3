eb8350c7389e359638a7acf84d1299a010152844
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 00:55:37 +0800

    add B2_prog01_not_equal_check

---
+++ B2_prog02_not_equal_check.py
@@ -0,0 +1,9 @@
+#Abegail Q. fernandez BSCPE1-4
+#B2_Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+if num1 != num2:
+  print("not equal")
+else:
+  print("equal")
