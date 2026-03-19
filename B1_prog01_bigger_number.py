1bbd9172def4e0f036607b5b6ddad57786645ced
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-19 21:58:35 +0800

    add B1_prog01_bigger_number

---
+++ B1_prog01_bigger_number.py
@@ -0,0 +1,10 @@
+#abegail Q. fernandez BSCPE1-4
+#B1_Prog01: Create a program that ask user to input 2 numbers. Print the bigger number. 
+
+num1 = int(input("enter first number: "))
+num2 = int(input("enter the second number: "))
+           
+if num1 < num2:
+    print(num2, "is the bigger number. ")
+else:
+    print(num1, "is the bigger number. ") 
