8768e9add168938ffb8f0fe9e41f160b3bfdc902
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 15:04:53 +0800

    add B5_prog05_proper_case

---
+++ B5_prog05_proper_case.py
@@ -0,0 +1,5 @@
+#Abegail Q. fernandez BSCPE1-4
+#B5_Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+name = input("enter fullname: ")
+print("fullname: ", name.title())
