f0bd4233b088b833a646f0acd7ea53e3d8ea95bd
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 14:43:40 +0800

    add B5_prog04_lowercase_name

---
+++ B5_prog04_lowercase_name.py
@@ -0,0 +1,5 @@
+#Abegail Q. fernandez BSCPE1-4
+#B5_Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+name = input("enter fullname: ")
+print("fullname: ", name.lower())
