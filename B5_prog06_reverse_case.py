eb9934a118060b624a7611108b62201faa29b4e2
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 15:12:17 +0800

    B5_prog06_reverse_case

---
+++ B5_prog06_reverse_case.py
@@ -0,0 +1,5 @@
+#Abegail Q. fernandez BSCPE1-4
+#B5_Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+name = input("Enter fullname: ")
+print("fullname: ", name.swapcase())
