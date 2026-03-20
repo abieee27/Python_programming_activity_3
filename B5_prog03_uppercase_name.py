c5d960cfc7286cd80720748e2fa6dffbb7dc7f74
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 14:37:11 +0800

    add B5_uppercase_name

---
+++ B5_prog03_uppercase_name.py
@@ -0,0 +1,5 @@
+#Abegail Q. fernandez BSCPE1-4
+#B5_Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+name = input("enter fullname: ")
+print("fullname:", name.upper())
