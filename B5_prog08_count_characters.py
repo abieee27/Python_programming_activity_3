79be1c6b7d2c8aaab3529065c091c86e050a53fb
abegailfernandez <abegailfernandez713@gmail.com>
2026-03-20 15:20:41 +0800

    add B5_prog08_count_characters

---
+++ B5_prog08_count_characters.py
@@ -0,0 +1,5 @@
+#Abegail Q. fernandez BSCPE1-4
+#B5_Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+name = input("Enter fullname: ")
+print(len(name))
